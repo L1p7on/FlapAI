54c18b414ad8dd095d8831e678b7ca05122df63b
Petr <ppritup2002@mail.ru>
2024-03-01 22:24:43 +0300

    formating

--- include/Dot.h
+++ include/Dot.h
@@ -1,71 +1,70 @@
 #include <SDL2/SDL.h>
-#include <vector>
+#include <algorithm>
 #include <iostream>
 #include <random>
-#include <algorithm>
 #include <utility>
-#include "Obstacle.h"
+#include <vector>
 #include "LTexture.h"
+#include "Obstacle.h"
 #ifndef DOT_H_
 #define DOT_H_
 extern LTexture gDotTexture;
-class Dot
-{
-    public:
-		//The dimensions of the dot
-		static const int DOT_WIDTH = 20;
-		static const int DOT_HEIGHT = 20;
+class Dot {
+ public:
+  // The dimensions of the dot
+  static const int DOT_WIDTH = 20;
+  static const int DOT_HEIGHT = 20;
 
-		//Maximum axis velocity of the dot
-		static const int DOT_VEL = 640;
+  // Maximum axis velocity of the dot
+  static const int DOT_VEL = 640;
 
-		//Initializes the variables
-		Dot();
+  // Initializes the variables
+  Dot();
 
-    bool end_game = false;
+  bool end_game = false;
 
-		//Takes key presses and adjusts the dot's velocity
-		void handleEvent( SDL_Event& e );
+  // Takes key presses and adjusts the dot's velocity
+  void handleEvent(SDL_Event& e);
 
-		//Moves the dot and checks collision
-		void move( float timeStep, std::vector<Obstacle>& walls );
+  // Moves the dot and checks collision
+  void move(float timeStep, std::vector<Obstacle>& walls);
 
-		//Shows the dot on the screen
-		void render();
+  // Shows the dot on the screen
+  void render();
 
-    bool checkCollision(const SDL_Rect& a, const SDL_Rect& b); 
+  bool checkCollision(const SDL_Rect& a, const SDL_Rect& b);
 
-    void restart();
+  void restart();
 
-    int score = 0;
+  int score = 0;
 
-    int length_path = 0;
+  int length_path = 0;
 
-    int number_of_neurons = 4;
+  int number_of_neurons = 4;
 
-    std::vector<std::vector<double> > weights_before_relu;
+  std::vector<std::vector<double> > weights_before_relu;
 
-    std::vector<double> weights_after_relu;
+  std::vector<double> weights_after_relu;
 
-    std::vector<double> auxiliary_matrix;
+  std::vector<double> auxiliary_matrix;
 
-    std::pair<int, int> dif (const std::vector<Obstacle>& walls);
+  std::pair<int, int> dif(const std::vector<Obstacle>& walls);
 
-    bool decision_making (const int diff_x, const int diff_y);
+  bool decision_making(const int diff_x, const int diff_y);
 
-    private:
-		//The X and Y offsets of the dot
-		float mPosX, mPosY;
+ private:
+  // The X and Y offsets of the dot
+  float mPosX, mPosY;
 
-		//The velocity of the dot
-		// int mVelX, mVelY;
-    float mVelX, mVelY;
-		float gravity = 300;
-		//Dot's collision box
-		SDL_Rect mCollider;
+  // The velocity of the dot
+  //  int mVelX, mVelY;
+  float mVelX, mVelY;
+  float gravity = 300;
+  // Dot's collision box
+  SDL_Rect mCollider;
 };
 
 int ReLU(double x);
 
-void Enfants(std::vector<Dot>& birds); // Enfants are children in french
-#endif // DOT_H_
+void Enfants(std::vector<Dot>& birds);  // Enfants are children in french
+#endif                                  // DOT_H_
--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -2,15 +2,15 @@
 Obstacle::Obstacle() {
   bottom.w = 40;
   top.w = 40;
-  bottom.x = SCREEN_WIDTH-bottom.w;
-  top.x = SCREEN_WIDTH-top.w;
-  bottom.y = SCREEN_HEIGHT-100;
+  bottom.x = SCREEN_WIDTH - bottom.w;
+  top.x = SCREEN_WIDTH - top.w;
+  bottom.y = SCREEN_HEIGHT - 100;
   top.y = 0;
   bottom.h = 100;
   top.h = 100;
   x_vel = 1;
   counted = false;
-  center = (bottom.y+top.h)/2;
+  center = (bottom.y + top.h) / 2;
   r = 0;
   g = 0;
   b = 0;
@@ -21,13 +21,13 @@ Obstacle::Obstacle(int r_bot_h, int r_top_h, int top_x, int bot_x) {
   top.w = 40;
   bottom.x = bot_x;
   top.x = top_x;
-  bottom.y = SCREEN_HEIGHT-r_bot_h;
+  bottom.y = SCREEN_HEIGHT - r_bot_h;
   top.y = 0;
   bottom.h = r_bot_h;
   top.h = r_top_h;
   x_vel = 1;
   counted = false;
-  center = (bottom.y + top.h)/2;
+  center = (bottom.y + top.h) / 2;
   r = 0;
   g = 0;
   b = 0;
@@ -40,19 +40,19 @@ void Obstacle::move() {
 
 void Obstacle::reset() {
   if (top.x + top.w < 0) {
-    int height; // rand a top of the hole in the wall
-    std::random_device rd;  // a seed source for the random number engine
-    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    int height;              // rand a top of the hole in the wall
+    std::random_device rd;   // a seed source for the random number engine
+    std::mt19937 gen(rd());  // mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<> distrib(100, 500);
     std::uniform_int_distribution<> color(0, 255);
     height = distrib(gen);
     top.x = SCREEN_WIDTH;
     bottom.x = SCREEN_WIDTH;
     top.h = height;
-    bottom.h = SCREEN_HEIGHT - top.h - 168; // 168 - height of the wall
-    bottom.y = SCREEN_HEIGHT-bottom.h;
+    bottom.h = SCREEN_HEIGHT - top.h - 168;  // 168 - height of the wall
+    bottom.y = SCREEN_HEIGHT - bottom.h;
     counted = false;
-    center = (bottom.y + top.h)/2;
+    center = (bottom.y + top.h) / 2;
     r = color(gen);
     g = color(gen);
     b = color(gen);
@@ -60,9 +60,9 @@ void Obstacle::reset() {
 }
 
 void Obstacle::render() {
-  SDL_SetRenderDrawColor( gRenderer, r, g, b, 0xFF );		
-	SDL_RenderFillRect( gRenderer, &bottom);
-	SDL_RenderFillRect( gRenderer, &top);
+  SDL_SetRenderDrawColor(gRenderer, r, g, b, 0xFF);
+  SDL_RenderFillRect(gRenderer, &bottom);
+  SDL_RenderFillRect(gRenderer, &top);
 }
 
 int Obstacle::GetVel() {
